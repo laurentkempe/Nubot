ba817fc1915dbb294e6088a3b930d6f10ee68c7e
Laurent Kempé <laurent.kempe@innoveo.com>
2014-07-17 07:59:17 +0200

    Handle stash deleting a branch

--- Nubot.Plugins.Samples/Stash/StashListener.cs
+++ Nubot.Plugins.Samples/Stash/StashListener.cs
@@ -47,15 +47,29 @@
 
         private string BuildMessage(StashModel model)
         {
-            var authorNames = model.Changesets.Values.Select(v => v.ToCommit.Author.Name).Distinct();
-            var branches = model.RefChanges.Select(r => r.RefId.Replace("refs/heads/", "")).Distinct();
+            var stringBuilder = new StringBuilder();
 
             var repositoryUrl = string.Format("{0}/projects/{1}/repos/{2}",
                                               Robot.Settings.Get("AtlassianStashUrl"),
-                                              model.Repository.Slug,
-                                              model.Repository.Project.Name);
+                                              model.Repository.Project.Name,
+                                              model.Repository.Slug);
 
-            var stringBuilder = new StringBuilder();
+            var branches = model.RefChanges.Select(r => r.RefId.Replace("refs/heads/", "")).Distinct();
+
+            if (model.RefChanges.Count() == 1 && model.RefChanges[0].Type.Equals("DELETE", StringComparison.InvariantCultureIgnoreCase))
+            {
+                stringBuilder
+                    .AppendFormat(
+                        "Branch <b>{0}</b> of <b>{1}/{2}</b> has been deleted<br/>",
+                        string.Join(", ", branches),
+                        model.Repository.Project.Name,
+                        model.Repository.Name);
+
+                return stringBuilder.ToString();
+            }
+
+            var authorNames = model.Changesets.Values.Select(v => v.ToCommit.Author.Name).Distinct();
+
             stringBuilder
                 .AppendFormat(
                     "<b>{0}</b> committed to {1} branch at <a href='{2}'>{3}/{4}</a><br/>",
@@ -81,14 +95,6 @@
             }
 
             return stringBuilder.ToString();
-        }
-
-        private string GetRepositoryUrl(StashModel model)
-        {
-            return string.Format("{0}/projects/{1}/repos/{2}",
-                Robot.Settings.Get("AtlassianStashUrl"),
-                model.Repository.Project.Name,
-                model.Repository.Slug);
         }
     }
 }
