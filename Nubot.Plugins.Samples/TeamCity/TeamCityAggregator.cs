98d251ff38a11a1a30125dbda10ba2ab3c78fe1a
Laurent Kempé <laurent.kempe@innoveo.com>
2014-08-20 09:44:55 +0200

    Split responsibility of TeamCityListener into TeamCityAggregator
    TeamCityListener is listening to Http post from a TeamCity server then Emit a message which TeamCityAggregator listen to and send a notification.

---
+++ Nubot.Plugins.Samples/TeamCity/TeamCityAggregator.cs
@@ -0,0 +1,39 @@
+﻿namespace Nubot.Plugins.Samples.TeamCity
+{
+    using System.ComponentModel.Composition;
+    using System.Text;
+    using Interfaces;
+    using Model;
+
+    [Export(typeof(IRobotPlugin))]
+    public class TeamCityAggregator : RobotPluginBase
+    {
+        [ImportingConstructor]
+        public TeamCityAggregator(IRobot robot)
+            : base("TeamCityAggregator", robot)
+        {
+            Robot.On<TeamCityModel>("TeamCityBuild", OnTeamCityBuild);
+        }
+
+        private void OnTeamCityBuild(object model)
+        {
+            Robot.SendNotification(
+                Robot.Settings.Get("TeamCityNotifyRoomName").Trim(),
+                Robot.Settings.Get("TeamCityHipchatAuthToken").Trim(),
+                BuildMessage((TeamCityModel)model),
+                true);
+        }
+
+        private string BuildMessage(TeamCityModel model)
+        {
+            var stringBuilder = new StringBuilder();
+
+            var buildStatusHtml = model.build.buildStatusHtml.Replace(@"<span class=""tcWebHooksMessage"">", "").Replace("</span>", "");
+
+            stringBuilder
+                .Append(buildStatusHtml);
+
+            return stringBuilder.ToString();
+        }
+    }
+}
